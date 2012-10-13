648a290d608b5f80f9e4d7d2e96986f0ff0ea91f
Eric Jang <ericjang2004@gmail.com>
2012-10-13 05:12:34 -0400

    labels are somehow working now! thanks to the funky transformation attr! d3 is hard...

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var connect = require('connect')
 		, host = 'localhost'
 		, article_cache = {}
 		, request = require('request')
-		, cheerio = require('cheerio');//for caching already-retrieved articles.
+		, cheerio = require('cheerio')
+		, agent_counter = 0;//for caching already-retrieved articles.
 	
 //Setup Express
 var server = express.createServer();
@@ -42,85 +43,132 @@ server.error(function(err, req, res, next){
 server.listen( port);
 
 
+function randomAgent(){
+	//cycle through user agents
+	
+	var agents = ['Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+	'( Robots.txt Validator http://www.searchengineworld.com/cgi-bin/robotcheck.cgi )',
+	'Amfibibot/0.07 (Amfibi Robot; http://www.amfibi.com; agent@amfibi.com) ',
+	'Googlebot/2.X (+http://www.googlebot.com/bot.html)',
+	'KIT-Fireball/2.0 libwww/5.0a',
+	'FyberSpider (+http://www.fybersearch.com/fyberspider.php)',
+	'ABCdatos BotLink/1.0.2 (test links)',
+	'Big Brother (http://pauillac.inria.fr/~fpottier/)',
+	'BigCliqueBOT/1.03-dev (bigclicbot; http://www.bigclique.com; bot@bigclique.com) ',
+	'BlogsNowBot, V 2.01 (+http://www.blogsnow.com/) '];
+	agent_counter += 1;
+	return agents[agent_counter % agents.length];
+	
+	//add more user agents later. that might help...
+}
+
+function parse_article($,parent_url) {
+	var article_limit = 30
+		,	children_dict = {}
+		, children = []
+		, rellinks = $('#mw-content-text .rellink a')//35 -> give priority to these nodes
+		, links = $('#mw-content-text p > a')
+		, numrellinks = rellinks.length
+		, numlinks = links.length;//27 <- be careful of order of selector and descendant annoying selectors
+		
+		if (rellinks.length !== 0) {
+			//add all the rellinks
+			for (var i = 0, ii = rellinks.length; i < ii; i++) {
+				var linkURL = $(rellinks[i]).attr('href');
+				var title = $(rellinks[i]).attr('title'); 
+							
+				//make sure the link begins with /wiki/
+				var re = new RegExp("^(/wiki/)");
+				
+				console.log(linkURL);
+				
+				//make sure it is not identical to parent url
+				if (linkURL !== parent_url && linkURL.match(re) !== null) {
+					children_dict[linkURL] = {'name':title, 'url':linkURL, 'childOf':parent_url, 'size':1000};//accounts for duplicates!
+				}	
+			}
+		}
+		
+		
+		if (links.length !== 0) {
+			//do something similar with the rest of the links but don't add any more if article has already > 30 links
+			for (var i = 0; (i < article_limit - numrellinks) && (i < links.length); i++) {
+				var linkURL = $(links[i]).attr('href');
+				//make sure the link begins with /wiki/
+				var re = new RegExp("^(/wiki/)");
+							
+				//make sure children are not identical to the url of its parent page it resides on
+				if (linkURL !== parent_url && linkURL.match(re) !== null) {
+					children_dict[linkURL] = {'name':title, 'url':linkURL, 'childOf':parent_url, 'size':1000};
+				}
+				
+			}
+			
+			//add all the children in the dict to the array
+			for (var i in children_dict) {
+				children.push(children_dict[i]);
+			}
+		}
+						
+		
+		var article = {
+				'name' : $('h1').text()
+			, 'url' : parent_url
+			, 'children' : children
+			, 'count' : numrellinks + numlinks//probably correlate to size later
+		};
+		
+		return article;//general article object. The search/random/get new children functions each make different assumptions on it.
+}
 
 //Setup Socket.IO
 var io = io.listen(server);
+
 io.sockets.on('connection', function(socket){
   console.log('Client Connected');
   
-	
-	socket.on('get articles', function(articles_array){
-		//articles requested. > general purpose handling for single article or even multiple.
-		//function takes in an array of article ids and returns whole article objects.
-		
-		//implement the cache storage logic later. Scrape Wikipedia for now.
-		
-		for (var i in articles_array) {
-			var article_url = articles_array[i];
-			request('http://en.wikipedia.org' + article_url, function (error, response, body) {
-			  if (!error && response.statusCode == 200) {
-			    
-					//parse it to make sure it is a valid structure > handle redirects if necessary
-					
-					var $ = cheerio.load(body);
-					
-					
-					//perhaps port this over to a function later.
-					
-					var article_limit = 30
-						,	children = {}
-						, rellinks = $('#mw-content-text .rellink a')//35 -> give priority to these nodes
-						, links = $('#mw-content-text p > a')
-						, numrellinks = rellinks.length
-						, numlinks = links.length;//27 <- be careful of order of selector and descendant annoying selectors
-					
-						//add all the rellinks
-						for (var i in rellinks) {
-							var linkURL = $(links[i]).attr('href');
-							var title = $(links[i]).attr('title'); 
-							
-							//make sure the link begins with /wiki/
-							var re = new RegExp("^(/wiki/)");
-							
-							//make sure it is not self
-							if (linkURL !== article_url && linkURL.match(re) !== null) {
-								children[linkURL] = {'title' : title};
-							}	
-						}
-						
-						//do something similar but don't add any more if article has already > 30 links
-						
-						for (var i = 0; i < article_limit - numrellinks; i++) {
-							var linkURL = $(links[i]).attr('href');
-							var title = $(links[i]).attr('title'); 
-							
-							//make sure the link begins with /wiki/
-							var re = new RegExp("^(/wiki/)");
-							
-							//make sure it is not self
-							if (linkURL !== article_url && linkURL.match(re) !== null) {
-								children[linkURL] = {'title' : title};
-							}
-							
-						}//end for loop
-						
-					debugger;
-					
-					var data = {
-							'title' : $('h1').text()
-						,	'childOf' : article_url
-						, 'children' : children
-						, 'count' : numrellinks + numlinks//probably correlate to size later
-					};
-					
-					//emit each article one at a time. Better asynchronous flow.
-					socket.emit('article',data);
-			  }
-			});
+	socket.on('search',function(query){
+		request({'uri':'http://en.wikipedia.org/wiki/Special:Search?search='+query+'&go=Go','headers':{'User-Agent':randomAgent()}},function(error,response,body){
 			
-		}
+			console.log(body);
+			//code here has to discern whether this is an actual page or not, in addition to the url
+			var $ = cheerio.load(body);
+			
+			//check if none found
+			if ($('.mw-search-nonefound').length !== 0) {
+				//none found! 
+				socket.emit('none found',query);
+			}
+			
+			var article = parse_article($,response.request.uri.href);
+			console.log(article.name);
+			socket.emit('article',article);//like the random article, send the whole thing.
+		});
 		
-	});	
+	});
+	
+	socket.on('get random article',function(){
+		request({'uri':'http://en.wikipedia.org/wiki/Special:Random','headers':{'User-Agent': randomAgent()}},function(error,response,body){
+			var article = parse_article(cheerio.load(body),response.request.uri.href);
+			socket.emit('article', article);//the whole article is emitted and upon loading in client, all children displayed immediately.
+		});
+	});
+	
+	socket.on('get new children', function(parent_url){
+		//fetch whole articles and send new leaf nodes back to client. this is much less computationally expensive and code-cluttering.
+		request('http://en.wikipedia.org' + parent_url, function(error,response,body){
+			debugger;
+			if (!error && response.statusCode == 200) {
+				var article = parse_article(cheerio.load(body),parent_url);
+				//now fetch new article nodes each child of that article and send them one by one. they will each get joined to the somewhat-empty leaf node in the client.
+				debugger;
+				for (var i in article.children) {
+					socket.emit('article',article.children[i]);
+				}
+				
+			}
+		});
+	});
 	
 	
 	
--- static/js/script.js
+++ static/js/script.js
@@ -3,6 +3,183 @@
 
 $(document).ready(function() {   
 
+	var width = 1000,
+	    height = 600,
+	    node,
+	    link,
+	    root,
+			first_node = true,
+			waiting_parents = {},
+			force,
+			viz;
+
+	function update() {
+	  nodes = flatten(root),//wasteful recursive function, but oh well.
+	  links = d3.layout.tree().links(nodes);
+
+	  // Restart the force layout.
+	  force
+	      .nodes(nodes)
+	      .links(links)
+				.linkDistance(150)
+				.charge(-10)
+	      .start();
+
+	  // Update the links…
+	  link = vis.selectAll("line.link")
+	      .data(links, function(d) { return d.target.id; });
+
+	  // Enter any new links.
+	  link.enter().insert("line", ".node")
+	      .attr("class", "link")
+	      .attr("x1", function(d) { return d.source.x; })
+	      .attr("y1", function(d) { return d.source.y; })
+	      .attr("x2", function(d) { return d.target.x; })
+	      .attr("y2", function(d) { return d.target.y; });
+
+	  // Exit any old links.
+	  link.exit().remove();
+
+	  // Update the nodes…
+	  node = vis.selectAll(".node")
+	      .data(nodes, function(d) { return d.id; })
+	      .style("fill", color);
+
+	  node.transition()
+	      .attr("r", function(d) { return d.children ? 4.5 : Math.sqrt(d.size) / 10; });
+
+	  // Enter any new nodes.
+	
+		node.enter().append("g")
+				.attr("class","node")
+		    .attr("cx", function(d) { return d.x; })
+		    .attr("cy", function(d) { return d.y; })
+				.call(force.drag);
+		
+		node.append("circle")
+				.attr("class", "node")
+		    .attr("r", function(d) { return d.children ? 4.5 : Math.sqrt(d.size) / 10; })
+		    .style("fill", color)
+		    .on("click", click);
+		
+	node.append("text")
+	      .attr("dx", 12)
+	      .attr("dy", ".35em")
+	      .text(function(d) { return d.name });
+		
+		/*
+		node.enter().append("circle")
+	  		.attr("class", "node")
+	      .attr("cx", function(d) { return d.x; })
+	      .attr("cy", function(d) { return d.y; })
+	      .attr("r", function(d) { return d.children ? 4.5 : Math.sqrt(d.size) / 10; })
+	      .style("fill", color)
+	      .on("click", click)
+	      .call(force.drag);
+		*/
+	
+	  // Exit any old nodes.
+	  node.exit().remove();
+	}
+			
+
+	function tick() {
+	  link.attr("x1", function(d) { return d.source.x; })
+	      .attr("y1", function(d) { return d.source.y; })
+	      .attr("x2", function(d) { return d.target.x; })
+	      .attr("y2", function(d) { return d.target.y; });
+
+		node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+	  //node.attr("cx", function(d) { return d.x; })
+	  //    .attr("cy", function(d) { return d.y; });
+	}
+
+	// Color leaf nodes orange, and packages white or blue.
+	function color(d) {
+	  return d._children ? "#3182bd" : d.children ? "#c6dbef" : "#fd8d3c";
+	}
+
+	// Toggle children on click.
+	function click(d) {
+	  if (d.children) {
+			//hiding - leave this be!
+	    d._children = d.children;
+	    d.children = null;
+	  } else if (d._children) {
+			//expansion of big bubble into smaller bubbles
+	    d.children = d._children;
+	    d._children = null;
+	  } else {
+	  	//no exposed or hidden children > must be a leaf node already!
+			console.log('expanding node...',d.url);
+			//save the listening parent node so that we can add children to it later.
+			d.children = [];
+			waiting_parents[d.url] = d;
+					
+			socket.emit('get new children',d.url);
+	  }
+		//run the update function!
+	  update();
+	}
+			
+	// Returns a list of all nodes under the root.
+	function flatten(root) {
+	  var nodes = [], i = 0;
+
+	  function recurse(node) {
+	    if (node.children) node.size = node.children.reduce(function(p, v) { return p + recurse(v); }, 0);
+	    if (!node.id) node.id = ++i;
+	    nodes.push(node);
+	    return node.size;
+	  }
+
+	  root.size = recurse(root);
+	  return nodes;
+	}
+			
+	function add_to_graph_old(node) {
+		// search for a 'clicked' node and append this node to its children, then call update();
+		//this seems kind of computationally expensive but it's probably the easiest solution
+		var parent_url = node.childOf;//the parent url we are looking for
+	
+		function find_daddy(base_node,_parent_url) {
+			console.log(base_node);
+			for (var i in base_node.children) {
+				//make sure we are getting the right one - might be duplicate names
+				if (base_node.children[i].url === _parent_url) {
+					console.log(base_node);
+					return base_node.children[i];
+				}
+				//didn't find it - search next layer recursively.
+				return find_daddy(base_node[i],_parent_url);
+			}
+		
+		}
+	
+		var daddy = find_daddy(root,parent_url);
+	
+		daddy.children.push(node);
+		update();
+	}
+			
+	function add_to_graph(node){
+		console.log('adding ',node.url)
+		for (var i in waiting_parents) {
+			if (node.childOf === waiting_parents[i].url) {
+				//found my parent! adding to parent should correctly add it to root node...
+				waiting_parents[i].children.push(node);//immediately visible
+				return 1;
+			}
+		}
+	}
+			
+	//quick test functions -> these work! make sure to set iswaiting and flip it off afterwards.
+	function test(){
+		root.children.push({'url':'google.com','size':1000,'iswaiting':true,'children':[]});
+		add_to_graph({'childOf':'google.com','size':1000});
+	}
+
+
 	var MODE = (function(){
 		var c = {};
 		c.modes = ['user','waiting','data'];
@@ -24,138 +201,15 @@ $(document).ready(function() {
 			$('#spacer').attr('style','height:10px')
 			$('#user').replaceWith('<div id="data" class="row-fluid"><div id="chart"></div></div>');
 			
-			var width = 1000,
-			    height = 600,
-			    node,
-			    link,
-			    root;
-
-			var force = d3.layout.force()
+			force = d3.layout.force()
 			    .on("tick", tick)
 			    .charge(function(d) { return d._children ? -d.size / 100 : -30; })
 			    .linkDistance(function(d) { return d.target._children ? 80 : 30; })
 			    .size([width, height]);
 
-			var vis = d3.select("#chart").append("svg")
+			vis = d3.select("#chart").append("svg")
 			    .attr("width", width)
 			    .attr("height", height);
-
-				
-				
-			//load the data as it comes instead of doing json.
-			
-			var root = {
-					fixed : true
-				, x : width / 2
-				, y : height / 2
-			}
-			
-			update();
-			
-			
-			function update() {
-			  var nodes = flatten(root),
-			      links = d3.layout.tree().links(nodes);
-
-			  // Restart the force layout.
-			  force
-			      .nodes(nodes)
-			      .links(links)
-			      .start();
-
-			  // Update the links…
-			  link = vis.selectAll("line.link")
-			      .data(links, function(d) { return d.target.id; });
-
-			  // Enter any new links.
-			  link.enter().insert("line", ".node")
-			      .attr("class", "link")
-			      .attr("x1", function(d) { return d.source.x; })
-			      .attr("y1", function(d) { return d.source.y; })
-			      .attr("x2", function(d) { return d.target.x; })
-			      .attr("y2", function(d) { return d.target.y; });
-
-			  // Exit any old links.
-			  link.exit().remove();
-
-			  // Update the nodes…
-			  node = vis.selectAll("circle.node")
-			      .data(nodes, function(d) { return d.id; })
-			      .style("fill", color);
-
-			  node.transition()
-			      .attr("r", function(d) { return d.children ? 4.5 : Math.sqrt(d.size) / 10; });
-
-			  // Enter any new nodes.
-			  node.enter().append("circle")
-			      .attr("class", "node")
-			      .attr("cx", function(d) { return d.x; })
-			      .attr("cy", function(d) { return d.y; })
-			      .attr("r", function(d) { return d.children ? 4.5 : Math.sqrt(d.size) / 10; })
-			      .style("fill", color)
-			      .on("click", click)
-			      .call(force.drag);
-
-			  // Exit any old nodes.
-			  node.exit().remove();
-			}
-
-			function tick() {
-			  link.attr("x1", function(d) { return d.source.x; })
-			      .attr("y1", function(d) { return d.source.y; })
-			      .attr("x2", function(d) { return d.target.x; })
-			      .attr("y2", function(d) { return d.target.y; });
-
-			  node.attr("cx", function(d) { return d.x; })
-			      .attr("cy", function(d) { return d.y; });
-			}
-
-			// Color leaf nodes orange, and packages white or blue.
-			function color(d) {
-			  return d._children ? "#3182bd" : d.children ? "#c6dbef" : "#fd8d3c";
-			}
-
-			// Toggle children on click.
-			function click(d) {
-			  if (d.children) {
-					//hiding - leave this be!
-			    d._children = d.children;
-			    d.children = null;
-			  } else if (d._children) {
-					//expansion of big bubble into smaller bubbles
-			    d.children = d._children;
-			    d._children = null;
-			  } else {
-			  	//no exposed or hidden children > must be a leaf node!
-					console.log('requesting new article...');
-					var articles_array = [];
-					
-					//TODO
-					socket.emit('get article',['ergot']);
-			  }
-				//run the update function!
-			  update();
-			}
-			
-			
-
-			// Returns a list of all nodes under the root.
-			//do i really need this function?
-			function flatten(root) {
-			  var nodes = [], i = 0;
-
-			  function recurse(node) {
-			    if (node.children) node.size = node.children.reduce(function(p, v) { return p + recurse(v); }, 0);
-			    if (!node.id) node.id = ++i;
-			    nodes.push(node);
-			    return node.size;
-			  }
-
-			  root.size = recurse(root);
-			  return nodes;
-			}
-			
-			
 		};
 		return c;
 	})();
@@ -175,38 +229,33 @@ $(document).ready(function() {
 	//bind submit functions to buttons (and enter key for search)
 	$('#search').bind('click', function(){
 		//get value of input, clear the input, and then emit it
-		var article = "anarchism";//TODO : remove this
-		socket.emit('search',article);
+		var query = $('#searchbox').val();
+		socket.emit('search',query);
 		MODE.set_waiting();
-		//temp:
-		MODE.set_data();
 	});
 	
 	$('#random').bind('click',function(){
-		var test_articles = ['Anarchism','Paul_Erdos'];
-		console.log('sending initial request...');
-		socket.emit('get articles',test_articles);
+		socket.emit('get random article','');
 		MODE.set_waiting();
 	});
 	
 	socket.on('article',function(article){
-		//if in user mode, ignore.
-		if (MODE.mode === "user") {return 0;}
-		if (MODE.mode === "waiting") { MODE.set_data();	return 0; }
+		//set into data mode if necessary
+		if (MODE.mode === "user" || MODE.mode === "waiting") {MODE.set_data();}
 		
-		//must be already in data mode. add article to the d3 layout
+		if (first_node) {//if node is first to be added, it becomes root
+			root = article;
+			first_node = false;
+			update();
+		} else {
+			add_to_graph(article);
+			update();
+		}
 		
-		/*
-		some things to check for : 
-		- if node is already in graph > TOO BAD, add it anyway 
-		
-		
-		
-		*/
-		
-		console.log('received article...', article);
 	});
 	
-	
+	socket.on('none found',function(query){
+		alert('none found for query ',query);
+	});
 	
 });
