a6d318afb29cccdb0e58533c1419fa38cd83eab9
Eric Jang <ericjang2004@gmail.com>
2012-10-12 22:06:29 -0400

    initial commit with some UI, most of the server assumably working, still tinkering with d3.js

--- server.js
+++ server.js
@@ -2,8 +2,12 @@
 var connect = require('connect')
     , express = require('express')
     , io = require('socket.io')
-    , port = (process.env.PORT || 8081);
-
+    , port = (process.env.PORT || 8081)
+		, host = 'localhost'
+		, article_cache = {}
+		, request = require('request')
+		, cheerio = require('cheerio');//for caching already-retrieved articles.
+	
 //Setup Express
 var server = express.createServer();
 server.configure(function(){
@@ -37,17 +41,94 @@ server.error(function(err, req, res, next){
 });
 server.listen( port);
 
+
+
 //Setup Socket.IO
 var io = io.listen(server);
 io.sockets.on('connection', function(socket){
   console.log('Client Connected');
-  socket.on('message', function(data){
-    socket.broadcast.emit('server_message',data);
-    socket.emit('server_message',data);
-  });
+  
+	
+	socket.on('get articles', function(articles_array){
+		//articles requested. > general purpose handling for single article or even multiple.
+		//function takes in an array of article ids and returns whole article objects.
+		
+		//implement the cache storage logic later. Scrape Wikipedia for now.
+		
+		for (var i in articles_array) {
+			var article_url = articles_array[i];
+			request('http://en.wikipedia.org' + article_url, function (error, response, body) {
+			  if (!error && response.statusCode == 200) {
+			    
+					//parse it to make sure it is a valid structure > handle redirects if necessary
+					
+					var $ = cheerio.load(body);
+					
+					
+					//perhaps port this over to a function later.
+					
+					var article_limit = 30
+						,	children = {}
+						, rellinks = $('#mw-content-text .rellink a')//35 -> give priority to these nodes
+						, links = $('#mw-content-text p > a')
+						, numrellinks = rellinks.length
+						, numlinks = links.length;//27 <- be careful of order of selector and descendant annoying selectors
+					
+						//add all the rellinks
+						for (var i in rellinks) {
+							var linkURL = $(links[i]).attr('href');
+							var title = $(links[i]).attr('title'); 
+							
+							//make sure the link begins with /wiki/
+							var re = new RegExp("^(/wiki/)");
+							
+							//make sure it is not self
+							if (linkURL !== article_url && linkURL.match(re) !== null) {
+								children[linkURL] = {'title' : title};
+							}	
+						}
+						
+						//do something similar but don't add any more if article has already > 30 links
+						
+						for (var i = 0; i < article_limit - numrellinks; i++) {
+							var linkURL = $(links[i]).attr('href');
+							var title = $(links[i]).attr('title'); 
+							
+							//make sure the link begins with /wiki/
+							var re = new RegExp("^(/wiki/)");
+							
+							//make sure it is not self
+							if (linkURL !== article_url && linkURL.match(re) !== null) {
+								children[linkURL] = {'title' : title};
+							}
+							
+						}//end for loop
+						
+					debugger;
+					
+					var data = {
+							'title' : $('h1').text()
+						,	'childOf' : article_url
+						, 'children' : children
+						, 'count' : numrellinks + numlinks//probably correlate to size later
+					};
+					
+					//emit each article one at a time. Better asynchronous flow.
+					socket.emit('article',data);
+			  }
+			});
+			
+		}
+		
+	});	
+	
+	
+	
   socket.on('disconnect', function(){
     console.log('Client Disconnected.');
   });
+	
+	
 });
 
 
@@ -60,12 +141,23 @@ io.sockets.on('connection', function(socket){
 server.get('/', function(req,res){
   res.render('index.jade', {
     locals : { 
-              title : 'Your Page Title'
-             ,description: 'Your Page Description'
-             ,author: 'Your Name'
+              title : 'Mutant Platato'
+             ,description: 'Wikipedia Knowledge Graph'
+             ,author: 'Eric Jang'
              ,analyticssiteid: 'XXXXXXX' 
             }
   });
+});
+
+
+server.get('/about',function(req,res){
+	res.render('about.jade',{
+		locals: {
+				title : 'Mutant Platato : About'
+			, description : 'About Page'
+			, author : 'Eric Jang'
+		}
+	});
 });
 
 
--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,212 @@
-/* Author: YOUR NAME HERE
+/* Author: Eric Jang
 */
 
 $(document).ready(function() {   
 
+	var MODE = (function(){
+		var c = {};
+		c.modes = ['user','waiting','data'];
+		c.mode = c.modes[0];
+		
+		c.set_user = function() {this.mode = c.modes[0];};
+		c.set_waiting = function() {
+			this.mode = c.modes[1];
+			//disable search box and replace #control div with ajax loader
+			$('#searchbox').attr('disabled','disabled');
+			$('#controls').replaceWith('<img src="images/ajax-loader.gif" style="display:block;margin:auto">');
+		};
+		c.set_data = function() {
+			this.mode = c.modes[2];
+			
+			//it would probably be convenient to make these variables accessible in a global scope for the socket.io to access
+			
+			//enter into data display mode
+			$('#spacer').attr('style','height:10px')
+			$('#user').replaceWith('<div id="data" class="row-fluid"><div id="chart"></div></div>');
+			
+			var width = 1000,
+			    height = 600,
+			    node,
+			    link,
+			    root;
+
+			var force = d3.layout.force()
+			    .on("tick", tick)
+			    .charge(function(d) { return d._children ? -d.size / 100 : -30; })
+			    .linkDistance(function(d) { return d.target._children ? 80 : 30; })
+			    .size([width, height]);
+
+			var vis = d3.select("#chart").append("svg")
+			    .attr("width", width)
+			    .attr("height", height);
+
+				
+				
+			//load the data as it comes instead of doing json.
+			
+			var root = {
+					fixed : true
+				, x : width / 2
+				, y : height / 2
+			}
+			
+			update();
+			
+			
+			function update() {
+			  var nodes = flatten(root),
+			      links = d3.layout.tree().links(nodes);
+
+			  // Restart the force layout.
+			  force
+			      .nodes(nodes)
+			      .links(links)
+			      .start();
+
+			  // Update the links…
+			  link = vis.selectAll("line.link")
+			      .data(links, function(d) { return d.target.id; });
+
+			  // Enter any new links.
+			  link.enter().insert("line", ".node")
+			      .attr("class", "link")
+			      .attr("x1", function(d) { return d.source.x; })
+			      .attr("y1", function(d) { return d.source.y; })
+			      .attr("x2", function(d) { return d.target.x; })
+			      .attr("y2", function(d) { return d.target.y; });
+
+			  // Exit any old links.
+			  link.exit().remove();
+
+			  // Update the nodes…
+			  node = vis.selectAll("circle.node")
+			      .data(nodes, function(d) { return d.id; })
+			      .style("fill", color);
+
+			  node.transition()
+			      .attr("r", function(d) { return d.children ? 4.5 : Math.sqrt(d.size) / 10; });
+
+			  // Enter any new nodes.
+			  node.enter().append("circle")
+			      .attr("class", "node")
+			      .attr("cx", function(d) { return d.x; })
+			      .attr("cy", function(d) { return d.y; })
+			      .attr("r", function(d) { return d.children ? 4.5 : Math.sqrt(d.size) / 10; })
+			      .style("fill", color)
+			      .on("click", click)
+			      .call(force.drag);
+
+			  // Exit any old nodes.
+			  node.exit().remove();
+			}
+
+			function tick() {
+			  link.attr("x1", function(d) { return d.source.x; })
+			      .attr("y1", function(d) { return d.source.y; })
+			      .attr("x2", function(d) { return d.target.x; })
+			      .attr("y2", function(d) { return d.target.y; });
+
+			  node.attr("cx", function(d) { return d.x; })
+			      .attr("cy", function(d) { return d.y; });
+			}
+
+			// Color leaf nodes orange, and packages white or blue.
+			function color(d) {
+			  return d._children ? "#3182bd" : d.children ? "#c6dbef" : "#fd8d3c";
+			}
+
+			// Toggle children on click.
+			function click(d) {
+			  if (d.children) {
+					//hiding - leave this be!
+			    d._children = d.children;
+			    d.children = null;
+			  } else if (d._children) {
+					//expansion of big bubble into smaller bubbles
+			    d.children = d._children;
+			    d._children = null;
+			  } else {
+			  	//no exposed or hidden children > must be a leaf node!
+					console.log('requesting new article...');
+					var articles_array = [];
+					
+					//TODO
+					socket.emit('get article',['ergot']);
+			  }
+				//run the update function!
+			  update();
+			}
+			
+			
+
+			// Returns a list of all nodes under the root.
+			//do i really need this function?
+			function flatten(root) {
+			  var nodes = [], i = 0;
+
+			  function recurse(node) {
+			    if (node.children) node.size = node.children.reduce(function(p, v) { return p + recurse(v); }, 0);
+			    if (!node.id) node.id = ++i;
+			    nodes.push(node);
+			    return node.size;
+			  }
+
+			  root.size = recurse(root);
+			  return nodes;
+			}
+			
+			
+		};
+		return c;
+	})();
+	
+	
+	
   var socket = io.connect();
-
-  $('#sender').bind('click', function() {
-   socket.emit('message', 'Message Sent on ' + new Date());     
-  });
-
-  socket.on('server_message', function(data){
-   $('#receiver').append('<li>' + data + '</li>');  
-  });
+	
+	$("[rel=tooltip]").tooltip({'placement':'bottom'});
+	
+	$('#searchbox').keypress(function(e){
+	    if (e.which == 13){
+	       $("#search").click();
+	    }
+	});
+	
+	//bind submit functions to buttons (and enter key for search)
+	$('#search').bind('click', function(){
+		//get value of input, clear the input, and then emit it
+		var article = "anarchism";//TODO : remove this
+		socket.emit('search',article);
+		MODE.set_waiting();
+		//temp:
+		MODE.set_data();
+	});
+	
+	$('#random').bind('click',function(){
+		var test_articles = ['Anarchism','Paul_Erdos'];
+		console.log('sending initial request...');
+		socket.emit('get articles',test_articles);
+		MODE.set_waiting();
+	});
+	
+	socket.on('article',function(article){
+		//if in user mode, ignore.
+		if (MODE.mode === "user") {return 0;}
+		if (MODE.mode === "waiting") { MODE.set_data();	return 0; }
+		
+		//must be already in data mode. add article to the d3 layout
+		
+		/*
+		some things to check for : 
+		- if node is already in graph > TOO BAD, add it anyway 
+		
+		
+		
+		*/
+		
+		console.log('received article...', article);
+	});
+	
+	
+	
 });
